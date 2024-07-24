a47d969f512f59977e3b18f81a0748727b90ef83
Abhijit <abhijitshendagescoe@gmail.com>
2024-07-24 18:43:46 +0530

    Removed Unnecessary code
    Deleted the old fixed code and everything

--- controllers/car_parks_controller.js
+++ controllers/car_parks_controller.js
@@ -1,10 +1,3 @@
-/***
-TODO:
-#759
-- refactor to use urls from lookup data/realtime-data-sources.json
-- convert to thin controller, with application logic in service layer
-***/
-
 const mongoose = require('mongoose');
 
 // Define schema with collection name
@@ -43,26 +36,3 @@ exports.getLatest = async (req, res, next) => {
 exports.getError = async (req, res, next) => {
   res.status(500).send('An error occurred');
 };
-
-// const getData = async url => {
-//   const fetch = require('node-fetch')
-//   try {
-//     const response = await fetch(url)
-//     const csv = await response.text()
-//     // console.log(csv)
-//     return csv
-//   } catch (error) {
-//     console.log(error)
-//     return error
-//   }
-// }
-
-// exports.getLatest = async (req, res, next) => {
-//   const url = 'https://data.corkcity.ie/datastore/dump/f4677dac-bb30-412e-95a8-d3c22134e3c0?bom=True'
-//   const response = await getData(url)
-//   res.send(response)
-// }
-
-// exports.getError = async (req, res, next) => {
-//   res.send()
-// }
